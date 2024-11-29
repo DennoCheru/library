3797617dc131cf5d16492779df50f80312a4ef18
Cheru <dennocheruh@gmail.com>
2024-11-30 02:58:24 +0300

    Add addBookToLibrary function
    Added a function to add a book to the myLibrary Array

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.bookInfo = function() {
-        return info = `${title} by ${author}, ${pages} pages, ${read}`
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}`
     };
 }
