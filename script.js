7dbf964fb3b464de93513b683892523681b9ca5f
Cheru <dennocheruh@gmail.com>
2024-12-06 20:53:37 +0300

    Refactor Book class
    Refactored the project to use a Book class intead
    of constructor.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const myLibrary = [];
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`
-    };
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
+    }
 }
+
+const myLibrary = [];
 
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
-
     myLibrary.push(newBook);
 }
 
